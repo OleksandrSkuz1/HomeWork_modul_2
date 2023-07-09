1cdea3dcf781a36ee4cb4c92ab5f034d56aa6741
OleksandrSkuz1 <138138803+OleksandrSkuz1@users.noreply.github.com>
2023-07-09 11:07:49 +0300

    modul2

---
+++ main.py
@@ -0,0 +1,45 @@
+if __name__ ==  "__main__":
+
+    # Завдання 1
+
+    # x = int(input("x: "))
+    # y = int(input("y: "))
+    # z = int(input("z: "))
+    # print(x, y, z, sep="")
+
+    # Завдання 2
+
+    # num1 = int(input("Введіть перше число: "))
+    # num2 = int(input("Введіть друге число: "))
+    # num3 = int(input("Введіть третє число: "))
+    # num4 = int(input("Введіть четверте число: "))
+    #
+    # print("Добуток чисел: ", num1 * num2 * num3 * num4)
+
+
+    # Завдання 3
+
+    # print("___ START PROGRAM ___ \n\n")
+    # system_metrics = input(f"Please choice system metrics:\n"
+    #                        f"m - Metry\n"
+    #                        f"dm - DMetry\n"
+    #                        f"mm - MMetry\n"
+    #                        f"myli - MMyli\n"
+    #                        f"Please choice: ")
+    # vidstan_value = int(input(f"Please input vidstan value:  "))
+    #
+    # # conditional temp
+    # if system_metrics.lower() == "m":
+    #     print(f"INPUT VALUE: {vidstan_value} M\n\n")
+    #     print(f"DMetry: {round(vidstan_value / 10, 2)}")                # конвертація метрів в дециметри
+    #     print(f"MMetry: {round(vidstan_value * 1000, 2)}")              # конвертація метрів в міліметри
+    #     print(f"MMyli: {round(vidstan_value * 0.00062137, 2)}")         # конвертація метрів в милі
+
+
+
+
+
+
+
+
+
